b23c155a62f3a6b30cc654e1049504fe1668dbe2
adevvv <adevvv@bk.ru>
2023-03-03 22:42:07 +0300

    Add: CustomViewClock library + class CustomViewClock + res + values strings

---
+++ customviewpointerclocklib/src/main/java/com/example/customviewpointerclocklib/CustomViewClock.kt
@@ -0,0 +1,14 @@
+package com.example.customviewpointerclocklib
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CustomViewClock @JvmOverloads constructor(
+    context: Context?,
+    attributeSet: AttributeSet? = null,
+    i: Int = 0
+) : View(context, attributeSet, i) {
+
+
+}
